2e2eccc8a31ffa6b214aaa5b1b8ae38e16a15429
Aya Shahin <66850776+AyaMShahin@users.noreply.github.com>
2023-03-06 22:32:58 +0200

    edited

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,9 @@
 /**
-* Template Name: Scaffold - v4.10.0
-* Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+ * Template Name: Scaffold - v4.10.0
+ * Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 (function () {
   "use strict";
 
@@ -112,28 +112,36 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function (e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function (e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function (e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
 
         let navbar = select("#navbar");
         if (navbar.classList.contains("navbar-mobile")) {
@@ -172,20 +180,25 @@
 
       let portfolioFilters = select("#portfolio-flters li", true);
 
-      on('click', '#portfolio-flters li', function (e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function (el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function () {
-          AOS.refresh()
-        });
-      }, true);
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          portfolioIsotope.on("arrangeComplete", function () {
+            AOS.refresh();
+          });
+        },
+        true
+      );
     }
   });
 
@@ -260,65 +273,39 @@ document.querySelector(".select-field").addEventListener("click", () => {
   document.querySelector(".down-arrow").classList.toggle("rotate180");
 });
 
-
-document.querySelector('.select-field').addEventListener('click', () => {
-  document.querySelector('.list').classList.toggle('show');
-  document.querySelector('.down-arrow').classList.toggle('rotate180');
-
+document.querySelector(".select-field").addEventListener("click", () => {
+  document.querySelector(".list").classList.toggle("show");
+  document.querySelector(".down-arrow").classList.toggle("rotate180");
 });
 
-document.querySelector('.select-field-2').addEventListener('click', () => {
-  document.querySelector('.list-2').classList.toggle('show');
-  document.querySelector('.down-arrow-2').classList.toggle('rotate180');
-
+document.querySelector(".select-field-2").addEventListener("click", () => {
+  document.querySelector(".list-2").classList.toggle("show");
+  document.querySelector(".down-arrow-2").classList.toggle("rotate180");
 });
 
-document.querySelector('.select-field-3').addEventListener('click', () => {
-  document.querySelector('.list-3').classList.toggle('show');
-  document.querySelector('.down-arrow-3').classList.toggle('rotate180');
-
+document.querySelector(".select-field-3").addEventListener("click", () => {
+  document.querySelector(".list-3").classList.toggle("show");
+  document.querySelector(".down-arrow-3").classList.toggle("rotate180");
 });
 
-document.querySelector('.select-field-4').addEventListener('click', () => {
-  document.querySelector('.list-4').classList.toggle('show');
-  document.querySelector('.down-arrow-4').classList.toggle('rotate180');
-
+document.querySelector(".select-field-4").addEventListener("click", () => {
+  document.querySelector(".list-4").classList.toggle("show");
+  document.querySelector(".down-arrow-4").classList.toggle("rotate180");
 });
 
-document.querySelector('.select-field-5').addEventListener('click', () => {
-  document.querySelector('.list-5').classList.toggle('show');
-  document.querySelector('.down-arrow-5').classList.toggle('rotate180');
-
+document.querySelector(".select-field-5").addEventListener("click", () => {
+  document.querySelector(".list-5").classList.toggle("show");
+  document.querySelector(".down-arrow-5").classList.toggle("rotate180");
 });
 
-document.querySelector('.select-field-6').addEventListener('click', () => {
-  document.querySelector('.list-6').classList.toggle('show');
-  document.querySelector('.down-arrow-6').classList.toggle('rotate180');
-
+document.querySelector(".select-field-6").addEventListener("click", () => {
+  document.querySelector(".list-6").classList.toggle("show");
+  document.querySelector(".down-arrow-6").classList.toggle("rotate180");
 });
 
-document.querySelector('.select-field-7').addEventListener('click', () => {
-  document.querySelector('.list-7').classList.toggle('show');
-  document.querySelector('.down-arrow').classList.toggle('rotate180');
-
-<<<<<<< Updated upstream
-$(document).ready(function () {
-  var scroll_start = 0;
-  var startchange = $("#hero");
-  var offset = startchange.offset();
-  if (startchange.length) {
-    $(document).scroll(function () {
-      scroll_start = $(this).scrollTop();
-      if (scroll_start > offset.top) {
-        $("#header").css("background-color", "#444444");
-        $("#header").style = "background";
-      } else {
-        $("#header").css("background-color", "transparent");
-      }
-    });
-  }
-=======
->>>>>>> Stashed changes
+document.querySelector(".select-field-7").addEventListener("click", () => {
+  document.querySelector(".list-7").classList.toggle("show");
+  document.querySelector(".down-arrow").classList.toggle("rotate180");
 });
 
 var next_1 = document.getElementById("next_1");
@@ -329,7 +316,6 @@ next_1.disabled = true;
 next_2.disabled = true;
 next_3.disabled = true;
 next_4.disabled = true;
-
 
 var in1 = document.getElementById("in1");
 var in2 = document.getElementById("in2");
@@ -345,103 +331,139 @@ var in11 = document.getElementById("in11");
 var in12 = document.getElementById("in12");
 var in13 = document.getElementById("in13");
 
+var check1 = document.getElementById("check1");
+var check2 = document.getElementById("check2");
+var check3 = document.getElementById("check3");
+var check4 = document.getElementById("check4");
+var check5 = document.getElementById("check5");
+var check6 = document.getElementById("check6");
+var check7 = document.getElementById("check7");
+var check8 = document.getElementById("check8");
+var check9 = document.getElementById("check9");
+var check10 = document.getElementById("check10");
+var check11 = document.getElementById("check11");
+var check12 = document.getElementById("check12");
+var text_area = document.getElementById("text_area");
 
-var check1=document.getElementById("check1");
-var check2=document.getElementById("check2");
-var check3=document.getElementById("check3");
-var check4=document.getElementById("check4");
-var check5=document.getElementById("check5");
-var check6=document.getElementById("check6");
-var check7=document.getElementById("check7");
-var check8=document.getElementById("check8");
-var check9=document.getElementById("check9");
-var check10=document.getElementById("check10");
-var check11=document.getElementById("check11");
-var check12=document.getElementById("check12");
-var text_area=document.getElementById("text_area");
-
-
-var flg=0;
-var flg2=0;
-
-
-
-
+var flg = 0;
+var flg2 = 0;
 
 [in1, in2, in3, in4, in5, in6].forEach(function (element) {
   element.addEventListener("input", function () {
-    if (in1.value != "" && in2.value != "" && in3.value != "" && in4.value != "" && in5.value != "" && in6.value != "") {
+    if (
+      in1.value != "" &&
+      in2.value != "" &&
+      in3.value != "" &&
+      in4.value != "" &&
+      in5.value != "" &&
+      in6.value != ""
+    ) {
       next_1.disabled = false;
-    }
-    else {
+    } else {
       next_1.disabled = true;
     }
-
   });
 });
-
-
 
 [check1, check2, check3, check4, check5, check6].forEach(function (element2) {
   element2.addEventListener("click", function () {
-    if (check1.checked||check2.checked||check3.checked||check4.checked||check5.checked||check6.checked) {
-      flg=1;
-      
+    if (
+      check1.checked ||
+      check2.checked ||
+      check3.checked ||
+      check4.checked ||
+      check5.checked ||
+      check6.checked
+    ) {
+      flg = 1;
+    } else {
+      flg = 0;
     }
-    else
-    {
-      flg=0;
-    }
-    
-
   });
 });
 
-[in7, in8, in9,check1,check2,check3,check4,check5,check6].forEach(function (element3) {
-  element3.addEventListener("input", function () {
-    if (in7.value != "" && in8.value != "" && in9.value != ""&&flg==1) {
-      next_2.disabled = false;
-    }
-    else {
-      next_2.disabled = true;
-    }
+[in7, in8, in9, check1, check2, check3, check4, check5, check6].forEach(
+  function (element3) {
+    element3.addEventListener("input", function () {
+      if (in7.value != "" && in8.value != "" && in9.value != "" && flg == 1) {
+        next_2.disabled = false;
+      } else {
+        next_2.disabled = true;
+      }
+    });
+  }
+);
 
-  });
-});
-
-[check7, check8, check9, check10, check11, check12].forEach(function (element4) {
+[check7, check8, check9, check10, check11, check12].forEach(function (
+  element4
+) {
   element4.addEventListener("click", function () {
-    if (check8.checked||check9.checked||check10.checked||check11.checked||check12.checked||check13.checked) {
-      flg2=1;
-      
+    if (
+      check8.checked ||
+      check9.checked ||
+      check10.checked ||
+      check11.checked ||
+      check12.checked ||
+      check13.checked
+    ) {
+      flg2 = 1;
+    } else {
+      flg2 = 0;
     }
-    else
-    {
-      flg2=0;
-    }
-    
-
   });
 });
 
-[in10, in11, in12,in13,check1,check7,check8,check9,check10,check11,check12].forEach(function (element5) {
+[
+  in10,
+  in11,
+  in12,
+  in13,
+  check1,
+  check7,
+  check8,
+  check9,
+  check10,
+  check11,
+  check12,
+].forEach(function (element5) {
   element5.addEventListener("input", function () {
-    if (in10.value != "" && in11.value != "" && in12.value != ""&& in13.value != ""&&flg2==1) {
+    if (
+      in10.value != "" &&
+      in11.value != "" &&
+      in12.value != "" &&
+      in13.value != "" &&
+      flg2 == 1
+    ) {
       next_3.disabled = false;
-    }
-    else {
+    } else {
       next_3.disabled = true;
     }
-
   });
 });
 
 text_area.addEventListener("input", function () {
   if (text_area.value != "") {
     next_4.disabled = false;
-  }
-  else {
+  } else {
     next_4.disabled = true;
   }
-
+  // <<<<<<< HEAD
 });
+
+$(document).ready(function () {
+  var scroll_start = 0;
+  var startchange = $("#hero");
+  var offset = startchange.offset();
+  if (startchange.length) {
+    $(document).scroll(function () {
+      scroll_start = $(this).scrollTop();
+      if (scroll_start > offset.top) {
+        $("#header").css("background-color", "#444444");
+        $("#header").style = "background";
+      } else {
+        $("#header").css("background-color", "transparent");
+      }
+    });
+  }
+});
+// >>>>>>> 6635aaa (edited)

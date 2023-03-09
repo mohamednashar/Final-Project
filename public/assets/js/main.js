7ea2cba48d038548768ecc95e7e251f262eebd7a
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2023-03-09 20:51:55 +0200

    Merge branch 'master' of https://github.com/mohamednashar/Final-Project

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -276,31 +276,31 @@ document.querySelector('.select-field').addEventListener('click', () => {
 
 document.querySelector('.select-field-2').addEventListener('click', () => {
   document.querySelector('.list-2').classList.toggle('show');
-  document.querySelector('.down-arrow-2').classList.toggle('rotate180');
+  document.querySelector('.down-arrow').classList.toggle('rotate180');
 
 });
 
 document.querySelector('.select-field-3').addEventListener('click', () => {
   document.querySelector('.list-3').classList.toggle('show');
-  document.querySelector('.down-arrow-3').classList.toggle('rotate180');
+  document.querySelector('.down-arrow').classList.toggle('rotate180');
 
 });
 
 document.querySelector('.select-field-4').addEventListener('click', () => {
   document.querySelector('.list-4').classList.toggle('show');
-  document.querySelector('.down-arrow-4').classList.toggle('rotate180');
+  document.querySelector('.down-arrow').classList.toggle('rotate180');
 
 });
 
 document.querySelector('.select-field-5').addEventListener('click', () => {
   document.querySelector('.list-5').classList.toggle('show');
-  document.querySelector('.down-arrow-5').classList.toggle('rotate180');
+  document.querySelector('.down-arrow').classList.toggle('rotate180');
 
 });
 
 document.querySelector('.select-field-6').addEventListener('click', () => {
   document.querySelector('.list-6').classList.toggle('show');
-  document.querySelector('.down-arrow-6').classList.toggle('rotate180');
+  document.querySelector('.down-arrow').classList.toggle('rotate180');
 
 });
 
